ee69f7bf8c3f2fc97ed0d9c1e7f9d2fe5facddf3
DmitriyLopatin <dmitriylopatin88@gmail.com>
2025-03-19 16:34:26 +0600

    wrapper-adaptive

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import HomePage from '../pages/HomePage/HomePage'
 function App() {
 
   return (
-    <main className='wrapper'>
+    <main className='max-w-[1440px] md:my-[40px] mx-auto my-[20px] md:p-[40px] p-[10px]'>
      <HomePage/>
     </main>
   )
